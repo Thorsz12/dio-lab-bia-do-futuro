d94c8daf17da73bc512512b5aa4e224311a4cdcf
Thorsz12 <thorsouzadosantos@gmail.com>
2026-04-19 21:21:32 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from agente import gerar_resposta
 
+st.title("💰 Assistente Financeiro")
+
+mensagem = st.text_input("Digite sua mensagem:")
+
+if mensagem:
+    resposta = gerar_resposta(mensagem)
+    st.write(resposta)
